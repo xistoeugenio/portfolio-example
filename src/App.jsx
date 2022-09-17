4aee874c5b98c6a87a7b67f22b9bb2d339b5db51
Xisto Eugenio Silva <xistoeugeniosilva@gmail.com>
2022-09-17 12:38:46 +0000

    "material.ui" and "react animated hamburguer"

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import Works from './components/works/Works';
 import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact';
 import './app.scss'
+import { useState } from "react";
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen =  {menuOpen} setMenuOpen = {setMenuOpen}/>
       <div className="sections">
-        <Intro/>
-        <Portfolio/>
-        <Works/>
+        <Intro />
+        <Portfolio />
+        <Works />
         <Testimonials />
-        <Contact/>
+        <Contact />
       </div>
     </div>
   );
