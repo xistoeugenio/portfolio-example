f86b41fa241cb183aaebbe57651cb8164aa41fc4
Xisto Eugenio Silva <xistoeugeniosilva@gmail.com>
2022-09-18 23:08:54 +0000

    "introduction page created" and "typing animation"

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <div className="app">
       <Topbar menuOpen =  {menuOpen} setMenuOpen = {setMenuOpen}/>
       <Menu menuOpen =  {menuOpen} setMenuOpen = {setMenuOpen}/>
-      <div className="sections">
+      <div className="sections" onClick={()=> setMenuOpen(false)}>
         <Intro />
         <Portfolio />
         <Works />
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,43 @@
+import { KeyboardDoubleArrowDown } from '@mui/icons-material'
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react'
 import './intro.scss'
 
 export default function Intro() {
-    return (
-        <div className='intro' id = "intro">
+    const texRef = useRef();
+    const render = useRef(true)
 
+
+    useEffect(() => {
+        if (render.current) {
+            render.current = false
+            init(texRef.current, {
+                showCursor: true,
+                backDelay: 1500,
+                backSpeed: 60,
+                strings: ["Developer", "Designer", "Content Creator"]
+            })
+        }
+    }, [])
+
+
+    return (
+        <div className='intro' id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/homem.png" alt='' />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Safak Kocaoglu</h1>
+                    <h3>Freelance <span ref={texRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <KeyboardDoubleArrowDown className='icon' />
+                </a>
+            </div>
         </div>
     )
 }
